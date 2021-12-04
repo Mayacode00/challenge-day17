df360031220432f5e869e6dc5c3e563220e090df
Mariam Ayoade <92252531+Mayacode00@users.noreply.github.com>
2021-12-04 23:23:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const bmiCalculator = document.getElementById("bmi-calculator");
+
+bmiCalculator.addEventListener("submit", calculateResults);
+
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const result = document.getElementById("result");
+
+function calculateResults(e) {
+  e.preventDefault();
+  console.log("calculating");
+
+  const heightInputed = parseFloat(height.value) / 100;
+  const convertedHeight = heightInputed * heightInputed;
+  const x = parseFloat(weight.value);
+
+  const resultCalculated = convertedHeight / x;
+
+  if (resultCalculated) {
+    result.value = resultCalculated.toFixed(2);
+  } else {
+    console.log("please check your numbers and try again");
+  }
+}
